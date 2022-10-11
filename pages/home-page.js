ec1a2933d38c8138cd6ab78e66dea0977b33d090
Austin Huizinga <42579548+austinth127@users.noreply.github.com>
2022-10-11 13:57:49 -0500

    make graph span page, make buttons absolute

--- pages/home-page.js
+++ pages/home-page.js
@@ -1,10 +1,5 @@
-import Link from "next/link";
 import React, { useRef } from "react";
-import ReactDom from "react-dom";
-import Head from "next/head";
 import Graph from "../components/graph/graphWrapper";
-import { ForceGraph3D } from "react-force-graph-3d";
-import myData from "../data/data.json";
 import { useRouter } from "next/router";
 
 // render the graph page
@@ -12,7 +7,7 @@ export default function MainPage() {
     const graphRef = useRef();
     const router = useRouter();
     return (
-        <div className="bg-black relative">
+        <div className="flex flex-row justify-center items-center w-full h-screen relative z-10 bg-black text-gray-50 overflow-x-clip">
             <h1 className="font-large leading-normal text-5xl text-green-800 absolute top-1 left-1">
                 Welcome Provenance Tracking Frog
             </h1>
@@ -23,15 +18,6 @@ export default function MainPage() {
             >
                 Return to Phrog Login
             </button>
-            {/* <div id="provDiv"></div> */}
         </div>
-        //<div className="grid grid-flow-row grid-cols-1 justify-center">
-        //  <h1 className= "font-large leading-normal text-5xl text-green-800">Welcome Provenance Tracking Frog</h1>
-        //<Link href="/">
-        //  <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-        //    Return to Phrog Login
-        //</button>
-        //</Link>
-        //</div>
     );
 }
